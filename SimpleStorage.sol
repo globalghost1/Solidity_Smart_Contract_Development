f31f1e800581ea03c170fca3cd7205cded09ae18
Patryk Ksiazek <globalghost1@protonmail.com>
2025-01-11 21:07:24 +0100

    Functions_2

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -4,10 +4,25 @@ pragma solidity ^0.8.19;  //Solidity Version
 
 contract SimpleStorage {
     //FavoriteNumber gets initilized to 0 if no value is given 
-    uint256 favoriteNumber = 0;
+    uint256 public favoriteNumber = 0;  // public, private, internal, external
 
-    function store()
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber; 
+        favoriteNumber = favoriteNumber + 1;
+        uint256 testVar = 5; 
+    }
+        
+    function something() public {
+            testVar = 6; //Can I do this? 
+            favoriteNumber = 7; // ?
+    }
     
-
+    // functions --> view
+    function retrive() public view returns(uint256){
+        return favoriteNumber;
+    }
+    // functions --> pure
+    //function retrive() public pure returs(uint256){
+    //   return 7;
+    
 }
-
