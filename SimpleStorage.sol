c4d07d1112eef353191617bf8712af410f47043c
Patryk Ksiazek <globalghost1@protonmail.com>
2025-01-11 20:38:35 +0100

    Variables_Functions_Solidity

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,13 +1,13 @@
 // SPDX-Licence-Identifier: MIT
 
-pragma solidity ^0.8.19;     //stating our version
+pragma solidity ^0.8.19;  //Solidity Version
 
 contract SimpleStorage {
+    //FavoriteNumber gets initilized to 0 if no value is given 
+    uint256 favoriteNumber = 0;
+
+    function store()
     
+
 }
-
-
-
-
-
 
