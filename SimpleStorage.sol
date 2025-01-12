f35edc8b114f02fb58a4395ba23e7e6e895a8bc3
Patryk Ksiazek <globalghost1@protonmail.com>
2025-01-12 20:08:44 +0100

    Arrays_Structs

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -6,6 +6,22 @@ contract SimpleStorage {
     //FavoriteNumber gets initilized to 0 if no value is given 
     uint256 public favoriteNumber = 0;  // public, private, internal, external
 
+    //uint256[] listOfFavoriteNumbers; // 0,1,2, [77, 68 , 90]
+    stuct Person {
+        uint256 favoriteNumer;
+        string name;
+    }
+
+    //Dynamic array
+    Person [] public listOfPeople; //[] --> Empty list
+    
+
+    // Person public pat = Person({favoriteNumber: 7, name: "Pat"});
+    // Person public Mariah = Person({favoriteNumber: 16, name: "Pat"});
+    // Person public Jon = Person({favoriteNumber: 12, name: "Pat"});
+
+
+
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber; 
         favoriteNumber = favoriteNumber + 1;
@@ -25,4 +41,8 @@ contract SimpleStorage {
     //function retrive() public pure returs(uint256){
     //   return 7;
     
+
+    function addPerson (string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push(Person(_favoriteNumber, _name));
+    }
 }
